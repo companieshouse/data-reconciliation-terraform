a1cc90d183b2bfe6622635f800b15d41f2688373
dgroves-ch <dgroves@companieshouse.gov.uk>
2021-06-02 16:02:01 +0100

    Add company status comparator crontab
    * New crontab for comparison between MongoDB and Elasticsearch primary
    index (company statuses).
    * Reorder comparison timings (for caching purposes).

--- groups/data-reconciliation/variables.tf
+++ groups/data-reconciliation/variables.tf
@@ -111,6 +111,11 @@ variable "company_name_mongo_alpha_crontab" {
   type = string
 }
 
+variable "company_status_mongo_primary_crontab" {
+  description = "A crontab expression that will be used to trigger a comparison of company statuses between MongoDB and the Elasticsearch primary index."
+  type = string
+}
+
 variable "company_profile_db" {
   description = "The name of the MongoDB database used to store company profile documents."
   type = string
